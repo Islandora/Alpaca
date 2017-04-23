1a737ea2a4dfdeeaffcb1b39972f201806bef847
Daniel Lamb <dlamb@islandora.ca>
2017-04-22 23:36:47 -0300

    Broke apart routes

--- islandora-indexing-fcrepo/src/main/java/ca/islandora/alpaca/indexing/fcrepo/FcrepoIndexer.java
+++ islandora-indexing-fcrepo/src/main/java/ca/islandora/alpaca/indexing/fcrepo/FcrepoIndexer.java
@@ -45,116 +45,22 @@ public class FcrepoIndexer extends RouteBuilder {
     @Override
     public void configure() {
 
-        // Global exception handler for the indexer.
-        // Just logs after retrying X number of times.
-        onException(Exception.class)
-            .maximumRedeliveries("{{error.maxRedeliveries}}")
-            .handled(true)
-            .log(
-                ERROR,
-                LOGGER,
-                "Error indexing ${property.uri} in fcrepo: ${exception.message}\n\n${exception.stacktrace}"
-            );
+        from("timer:foo?period=5000")
+                .log(INFO, LOGGER, "HELLO");
 
-        // Main router.
-        from("{{input.stream}}")
-            .routeId("IslandoraFcrepoIndexerRouter")
-              .to("direct:parse.event")
-              .choice()
-                .when(exchangeProperty("action").isEqualTo("Delete"))
-                  .to("direct:fcrepo.delete")
-                .otherwise()
-                  .to("direct:retrieve.resource")
-                  .choice()
-                    .when(exchangeProperty("action").isEqualTo("Create"))
-                      .to("direct:fcrepo.create")
-                  .endChoice()
-                .endChoice();
-                /*
-                  .otherwise()
-                    .to("direct:fcrepo.update");
-                    */
-
-        // Extracts info using jsonpath and stores it as properties on the exchange.
-        from("direct:parse.event")
-            .routeId("IslandoraFcrepoIndexerParseEvent")
-              // Custom exception handler.  Doesn't retry if event is malformed.
-              .onException(JsonPathException.class)
-                .maximumRedeliveries(0)
-                .handled(true)
-                .log(
-                   ERROR,
-                   LOGGER,
-                   "Error extracting properties from event: ${exception.message}\n\n${exception.stacktrace}"
-                )
-                .end()
-              .setProperty("action").jsonpath("$.type")
-              .setProperty("uri").jsonpath("$.object");
-
-        // Retrieves the resource from Drupal.
-        from("direct:retrieve.resource")
-            .routeId("IslandoraFcrepoIndexerRetrieveResource")
-            .log(INFO, LOGGER, "GETTING RESOURCE")
-            .setHeader(Exchange.HTTP_METHOD, constant("GET"))
-            .toD("${property.uri}?_format=jsonld&authUsername={{drupal.username}}" +
-                    "&authPassword={{drupal.password}}"
-            )
-            .unmarshal().json(Jackson);
-        /*
-            .setBody().jsonpath("$.['@graph']")
-            .unmarshal().json(Jackson, List.class)
-            .split().simple("${body}")
-                .log(INFO, LOGGER, "ID: ${body[@id]}");
-                */
-
-
-        /*
-        from("direct:retrive.id")
-                .routeId("IslandoraFcrepoIndexerRetrieveId")
-                .process(exchange -> {
-                    String regex = "fedora_resource\\/(\\d)";
-                    Pattern pattern = Pattern.compile(regex);
-                    Matcher matcher = pattern.matcher(exchange.getProperty("uri", String.class));
-                    if (matcher.find()) {
-                        exchange.setProperty("id", matcher.group());
-                    }
-                    else {
-                        throw new RuntimeException("OH NOES");
-                    }
-                })
-                .log(INFO, LOGGER, "Got an ID: ${property.id}");
-                //.setHeader(Exchange.HTTP_METHOD, constant("GET"))
-                //.toD("{{idiomatic.baseUrl}}/${property.id}");
-           */
-        // POSTs a SPARQL delete query for all triples with subject == uri.
-        from("direct:fcrepo.delete")
+        from("{{delete.input.stream")
             .routeId("IslandoraFcrepoIndexerDelete")
-                .log(INFO, LOGGER, "DELETE EVENT");
-        /*
-              .setHeader(FCREPO_URI, simple("${property.uri}?_format=jsonld"))
-              .process(new SparqlDeleteProcessor())
-              .log(INFO, LOGGER, "Deleting ${property.uri} in triplestore")
-              .to("{{triplestore.baseUrl}}");
-              */
+                .log(INFO, LOGGER, "DELETE EVENT")
+                .to("{{delete.output.stream}}");
 
-        from("direct:fcrepo.create")
+        from("{{create.input.stream}}")
             .routeId("IslandoraFcrepoIndexerCreate")
                 .log(INFO, LOGGER, "CREATE")
-                .removeHeaders("*")
-                .setHeader(Exchange.HTTP_METHOD, constant("POST"))
-                .setHeader(Exchange.CONTENT_TYPE, constant("application/ld+json"))
-                //.to("fcrepo:{{fcrepo.baseUrl}}")
-                .log(INFO, LOGGER, "${body}");
+                .to("{{create.output.stream}}");
 
-
-        from("direct:fcrepo.update")
+        from("{{update.input.stream}}")
                 .routeId("IslandoraFcrepoIndexerUpdate")
-                .log(INFO, LOGGER, "UPDATE");
-        /*
-              .setHeader(FCREPO_URI, simple("${property.uri}?_format=jsonld"))
-              .process(new SparqlUpdateProcessor())
-              .log(INFO, LOGGER, "Indexing ${property.uri} in triplestore")
-              .to("{{triplestore.baseUrl}}");
-              */
+                .log(INFO, LOGGER, "UPDATE")
+                .to("{{update.output.stream}}");
     }
 }
